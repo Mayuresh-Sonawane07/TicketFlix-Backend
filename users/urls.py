412dc1be8aff3752c09cee6483eaf480e6c19086
Mayur <55075877-mayuresh1526@users.noreply.replit.com>
2026-03-19 14:47:27 +0000

    Implement proper JWT authentication with simplejwt

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenBlacklistView
 from .views import (
     UserViewSet,
     RegisterInitView,
