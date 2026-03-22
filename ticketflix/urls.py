46f0bce338308f2f67b7a58a83b23e0d7b19618d
Mayur <55075877-mayuresh1526@users.noreply.replit.com>
2026-03-22 19:50:06 +0000

    Remove debug endpoint

--- ticketflix/urls.py
+++ ticketflix/urls.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
-from django.urls import re_path
-from django.http import JsonResponse
-from django.core.files.storage import default_storage
-import os
-
-def storage_debug(request):
-    return JsonResponse({
-        'storage_class': str(default_storage.__class__),
-        'cloud_name': os.environ.get('CLOUD_NAME'),
-        'api_key': os.environ.get('API_KEY'),
-        'api_secret_set': bool(os.environ.get('API_SECRET')),
-    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +11,4 @@ urlpatterns = [
     path('api/bookings/', include('bookings.urls')),
     path('api/payments/', include('payments.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    path('debug/storage/', storage_debug),
 ]
