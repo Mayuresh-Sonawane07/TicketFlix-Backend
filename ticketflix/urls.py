5e0672a3a2d53f2adf2a69536e59847fad877f83
Mayur <55075877-mayuresh1526@users.noreply.replit.com>
2026-03-22 19:00:10 +0000

    Add storage debug

--- ticketflix/urls.py
+++ ticketflix/urls.py
@@ -6,6 +6,10 @@ from django.conf import settings
 from django.views.static import serve
 from django.urls import re_path
 
+from django.http import JsonResponse
+from django.core.files.storage import default_storage
+import os
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
@@ -14,4 +18,13 @@ urlpatterns = [
     path('api/bookings/', include('bookings.urls')),
     path('api/payments/', include('payments.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('debug/storage/', storage_debug),
 ]
+
+def storage_debug(request):
+    return JsonResponse({
+        'storage_class': str(default_storage.__class__),
+        'cloud_name': os.environ.get('CLOUD_NAME'),
+        'api_key': os.environ.get('API_KEY'),
+        'api_secret_set': bool(os.environ.get('API_SECRET')),
+    })
