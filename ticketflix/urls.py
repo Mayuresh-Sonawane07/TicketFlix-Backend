e9d42fe9c470384f34a652794c1abf550c74084e
Mayur <55075877-mayuresh1526@users.noreply.replit.com>
2026-03-22 19:02:58 +0000

    Add storage debug

--- ticketflix/urls.py
+++ ticketflix/urls.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-# from django.conf.urls.static import static
-
 from django.views.static import serve
 from django.urls import re_path
-
 from django.http import JsonResponse
 from django.core.files.storage import default_storage
 import os
+
+def storage_debug(request):
+    return JsonResponse({
+        'storage_class': str(default_storage.__class__),
+        'cloud_name': os.environ.get('CLOUD_NAME'),
+        'api_key': os.environ.get('API_KEY'),
+        'api_secret_set': bool(os.environ.get('API_SECRET')),
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,11 +25,3 @@ urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('debug/storage/', storage_debug),
 ]
-
-def storage_debug(request):
-    return JsonResponse({
-        'storage_class': str(default_storage.__class__),
-        'cloud_name': os.environ.get('CLOUD_NAME'),
-        'api_key': os.environ.get('API_KEY'),
-        'api_secret_set': bool(os.environ.get('API_SECRET')),
-    })
