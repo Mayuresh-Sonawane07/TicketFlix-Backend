7aa59605f7fd0faba0af68158a56974987be20c4
Mayuresh Sonawane <mayureshsonawane1526@gmail.com>
2026-04-04 02:54:19 +0530

    Admin

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.throttling import UserRateThrottle
+from rest_framework.pagination import PageNumberPagination
 
 class AdminThrottle(UserRateThrottle):
     scope = 'admin'
