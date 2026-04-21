eadb74bbe6d8c1e304a930528a3f655b968bc0f5
Mayuresh Sonawane <mayureshsonawane1526@gmail.com>
2026-04-21 22:30:44 +0530

    Clean pycache, fix gitignore and update backend changes

--- ticketflix/urls.py
+++ ticketflix/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
-from payments.views import TestGmailTokenView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +10,6 @@ urlpatterns = [
     path('api/theaters/', include('theaters.urls')),
     path('api/bookings/', include('bookings.urls')),
     path('api/payments/', include('payments.urls')),
-    path('api/debug/gmail-token/', TestGmailTokenView.as_view()),
     path('api/admin-panel/', include('adminpanel.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
