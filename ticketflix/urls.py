08ed22a5b90a68c0962f4790a064659efc1e9294
Mayuresh Sonawane <mayureshsonawane1526@gmail.com>
2026-04-21 21:25:50 +0530

    Added temporary debug

--- ticketflix/urls.py
+++ ticketflix/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('api/theaters/', include('theaters.urls')),
     path('api/bookings/', include('bookings.urls')),
     path('api/payments/', include('payments.urls')),
-    path('api/debug/gmail-token/', TestGmailTokenView()),
+    path('api/debug/gmail-token/', TestGmailTokenView.as_view()),
     path('api/admin-panel/', include('adminpanel.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
